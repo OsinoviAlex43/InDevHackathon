01b4c14e285cf3d0bcc2985e9d42766015e6cac2
Астахов Артём <artemastahov27@gmail.com>
2025-05-16 13:00:01 +0300

    create ws-config

---
+++ backend/java/HacathonBack/src/main/java/by/osinovii/hacathonback/services/api/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package by.osinovii.hacathonback.services.api;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/action")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user", "/admin");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
