37e1e64d9ee6254d4084816b222d6cdec1099fae
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-05-16 11:43:10 +0300

    Project initialize

---
+++ backend/java/HacathonBack/src/main/java/by/osinovii/hacathonback/HacathonBackApplication.java
@@ -0,0 +1,13 @@
+package by.osinovii.hacathonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacathonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HacathonBackApplication.class, args);
+    }
+
+}
